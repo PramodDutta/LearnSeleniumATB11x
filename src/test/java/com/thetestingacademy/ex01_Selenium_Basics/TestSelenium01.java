cfab89c6f8873e25f4ed31eeabeb8d9b1ae3a274
Promode <pramoddutta_96@yahoo.com>
2025-05-16 06:59:23 +0530

    16th May

--- src/test/java/com/thetestingacademy/ex01_Selenium_Basics/TestSelenium01.java
+++ src/test/java/com/thetestingacademy/ex01_Selenium_Basics/TestSelenium01.java
@@ -1,2 +1,19 @@
-package com.thetestingacademy.ex01_Selenium_Basics;public class TestSelenium01 {
+package com.thetestingacademy.ex01_Selenium_Basics;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium01 {
+
+    @Test
+    public void test_openVWOLoginPage(){
+        FirefoxDriver firefoxDriver = new FirefoxDriver();
+        firefoxDriver.get("https://app.vwo.com");
+        firefoxDriver.quit();
+
+
+    }
+
+
 }
